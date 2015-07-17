9c42d94050e61a1b8f181bb9e280074a099b56ec
Pandada8 <pandada8@gmail.com>
2015-07-18 00:53:05 +0800

    Add the middleware

--- show/__init__.py
+++ show/__init__.py
@@ -2,4 +2,6 @@ class ReversedProxyMiddleware():
     def process_request(self, request):
         if "X-SCRIPT-NAME" in request.META and request.META['X-SCRIPT-NAME']:
             script_name = request.META['X-SCRIPT-NAME']
-            if 
+            print(request.path_info, script_name)
+            if script_name and request.path_info.startswith(script_name):
+                request.path_info = request.path_info[len(script_name):]
